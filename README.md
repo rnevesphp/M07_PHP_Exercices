12ada21fbfa2547379b4e1956958e21a9a6a055a
Romeu <romeu.esp@gmail.com>
2021-10-31 02:31:14 +0200

    :wrench:

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# M07_PHP_DesEnSer
+# PHP - Server Side Develeopment
+
+## This repository was created in order to upload the codes with the evolution of our work in class with the PHP language in Server environment. 
---
+++ UF1/ex_001.php
@@ -0,0 +1,32 @@
+<?php
+   // Definimos una variable "nombre" que alamacenará el nombre de un alumno como valor
+   $nombre = "Romeu"; 
+   // Las Variables `primerAp` y `segundoAp` almacenarán los apellidos de cada alumno
+   $primerAp = "Neves"; 
+   $segundoAp = "Rabelo"; 
+   // La variable `ageAlumno` almacena su edad como valor 
+   $ageAlumno = "20"; 
+   // La variable `fechaNac` almacena la fecha de nacimiento del alumno 
+   $fechaNac = "21/05/2001";
+   // La variable `telefonoAlumno` almacena su numero de tlf
+   $telefonoAlumno = 665859515; 
+   // La variable `correoAlumno` alamcena el correo del alumno como valor 
+   $correoAlumno = "nevesromeu21@gmail.com"; 
+   // La variable `` almacena si el alumno trabaja o no 
+   $actTrabajando = bool ; 
+   // La variable ``
+   $altura = 1.84; 
+   // La variable ``
+   $localidadPueblo = "St. Boi de Llobregat"; 
+
+   echo("<p>Me llamo " . $nombre); 
+   echo($primerAp); 
+   echo($segundoAp); 
+   echo($ageAlumno); 
+   echo($fechaNac); 
+   echo($telefonoAlumno); 
+   echo($correoAlumno); 
+   echo($actTrabajando); 
+   echo($altura);
+   echo($localidadPueblo);
+?>
