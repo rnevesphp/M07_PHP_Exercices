3b11bc9f0ad7fad65331826445c3376d81aa93f4
Romeu <romeu.esp@gmail.com>
2021-11-03 17:22:41 +0100

    :wrench:

--- UF1/act_003.php
+++ UF1/act_003.php
@@ -28,15 +28,23 @@
 
    // Ejercicio 3D - Factorial  // 
     echo("<h3>Factorial de un numero</h3> "); 
-    $numero = 7;  
-    function CalFactorial($numero){
-        $total = 1; 
-        for($i = $numero; $i >= 1; $i--){
-          $total = $total*$i;
-          echo("Numero decreciente: ".$i."<br>"); 
-        }
-      return $total;
-    }
+    $numero = 4;  // Creamos una variable y le atribuimos un valor 
+    
+    function CalFactorial($numero){ // Creamos una funcion que hará el calculo del factorial
+      $total = 1 ; //creamos una variable total y le damos el valor de 1
+      for($iCont = $numero; $iCont >= 1; $iCont--){
+          // a la nueva variable iCont le atribuimos el valor del numero introducido 
+          // para iCont siendo mayor que 1 pues el por cada vez que el bucle repita se le decrementará un numero
+          
+          //Para cada iteración del bucle multiplcamos el valor de iCont por el total que devolveria iCont*1
+          $total = $total * $iCont; 
 
-    echo("El factorial del numero ".$numero." es: ".CalFactorial($numero)) ; // Imprimos el valor del factorial del numero introducido
+          // Para cada iteración del bucle sacamos el valor del numero introducido decrementado
+          echo("Numero decreciente: ".$iCont."<br>"); 
+        }
+        return $total; //La función debe de devolver un total; 
+    }
+    // Imprimos el valor del factorial del numero introducido
+    echo("El factorial del numero ".$numero." es: ".CalFactorial($numero)) ; 
+    
 ?> 
--- UF1/act_004.php
+++ UF1/act_004.php
@@ -1,4 +1,7 @@
 <?php
-   $listaDeProductos = array("Switch", "Portatil", "Torre Gaming", "XBOX", "Playstation" )
-   
+   $listaDeProductos = array("Switch", "Portatil", "Torre Gaming", "XBOX", "Playstation" ); 
+   $gastosEnvioProductos = array(15, 30, 50, 35, 45); 
+   $descuentoProductos = array(10, 20, 30, 25, 25)
+
+   $totalFinalProducto; 
 ?> 
